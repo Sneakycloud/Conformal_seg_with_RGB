f37061cc14cb9e76d6201e5f18d518c8f35c32c2
Vencilo <axbe701@gmail.com>
2026-05-18 16:54:58 +0200

    Fixed a bit with data_handling.

--- src/utils2/data_handling.py
+++ src/utils2/data_handling.py
@@ -1,360 +1,141 @@
+import os
+import cv2
+import logging
 import numpy as np
 import tifffile
-
-class ToTensor:
-    def __call__(self, sample):
-        # Convert inputs (RGB, Depth) to float tensors
-        inputs = [torch.from_numpy(img.transpose((2, 0, 1))).float() for img in sample['inputs']]
-        
-        # FIX: Ensure target is a LongTensor containing class indices, not one-hot maps
-        target = torch.from_numpy(sample['target']).long() 
-        
-        return {'inputs': inputs, 'target': target}
-
-def calculate_mfb_weights(target_paths, classes):
-    class_counts = {c: 0 for c in classes}
-    class_totals = {c: 0 for c in classes}
-
-    for target_path in target_paths:
-        target = tifffile.imread(target_path)
-        target_pixels = np.prod(target.shape)
-        for cls_idx in classes:
-            class_counts[cls_idx] += np.sum(target == cls_idx)
-            if (target == cls_idx).any():
-                class_totals[cls_idx] += target_pixels
-
-    frequencies = []
-    for cls_idx in classes:
-        cnt = class_counts[cls_idx]
-        tot = class_totals[cls_idx]
-        if cnt == 0 and tot == 0:
-            frequencies.append(0.0001)  # Stability fallback
-        else:
-            frequencies.append(cnt / tot)
-
-    # Median frequency balancing equation
-    weights = [np.median(frequencies) / freq for freq in frequencies]
-    return np.array(weights, dtype=np.float32)
+import torch
+from torch.utils.data import Dataset
 
 class RandomFlip:
-
-    '''Randomly apply flip transform to inputs and target'''
+    """Randomly applies a horizontal/vertical flip to inputs and target simultaneously"""
+    def __init__(self, rng):
+        self._rng = rng
 
     @classmethod
     def _flip_image(cls, image, flip_code):
-        '''Flip given image according to given flip code
-
-        Parameters
-        ----------
-        image : Image to flip
-        flip_code : Indication how to flip the image
-
-        Returns
-        -------
-        Flipped image
-
-        '''
         if flip_code is None:
             return image
-
         return cv2.flip(image, flip_code)
 
-    def __init__(self, rng):
-        '''Setup flip transform
-
-        Parameters
-        ----------
-        rng : Random number generator to use
-
-
-        '''
-        self._rng = rng
-
     def __call__(self, sample):
-        '''Apply flip transform
-
-        Parameters
-        ----------
-        sample : Sample dictionary of form: {'inputs': [ndarray, ...],
-                 'target': ndarray}
-
-        Returns
-        -------
-        Transformed sample
-
-        '''
         flip_code = self._rng.choice([0, 1, -1, None])
         transformed_inputs = []
         for original_input in sample['inputs']:
-            transformed_inputs.append(self._flip_image(original_input,
-                                                       flip_code))
+            transformed_inputs.append(self._flip_image(original_input, flip_code))
 
         transformed_target = self._flip_image(sample['target'], flip_code)
-
-        return {'inputs': transformed_inputs, 'target': transformed_target,
-                'id': sample['id']}
+        return {'inputs': transformed_inputs, 'target': transformed_target}
 
 
 class RandomRotate:
-
-    '''Apply random rotation to inputs and target images'''
-
-    @classmethod
-    def _warp_img(cls, image, transform, discretize):
-        '''Warp given image based on given transform
-
-        Parameters
-        ----------
-        image : Image to warp
-        transform : Transformation matrix
-        discretize : Indicate if resulting values should be discretized
-
-        Returns
-        -------
-        Warped image
-
-        '''
-        y, x = image.shape[:2]
-
-        border_value = 0
-        warped_img = cv2.warpAffine(image, transform, dsize=(x, y),
-                                    borderValue=border_value)
-        if discretize:
-            warped_img = np.round(warped_img)
-
-        return warped_img
-
+    """Randomly rotates inputs and target simultaneously by 90, 180, or 270 degrees"""
     def __init__(self, rng):
-        '''Setup rotation transform
-
-        Parameters
-        ----------
-        rng : Random number generator to use
-
-
-        '''
         self._rng = rng
 
+    @classmethod
+    def _rotate_image(cls, image, code):
+        if code is None:
+            return image
+        return cv2.rotate(image, code)
+
     def __call__(self, sample):
-        '''Apply rotation transform
-
-        Parameters
-        ----------
-        sample : Sample dictionary of form: {'inputs': [ndarray, ...],
-                 'target': ndarray}
-
-        Returns
-        -------
-        Transformed sample
-
-        '''
-        angle = self._rng.choice([0, 90, 180, 270])
-        center = np.array(sample['target'].shape) / 2
-        transform_matrix = cv2.getRotationMatrix2D(tuple(center), angle, 1)
-
+        code = self._rng.choice([cv2.ROTATE_90_CLOCKWISE, 
+                                 cv2.ROTATE_180, 
+                                 cv2.ROTATE_90_COUNTERCLOCKWISE, 
+                                 None])
         transformed_inputs = []
         for original_input in sample['inputs']:
-            transformed_inputs.append(self._warp_img(original_input,
-                                                     transform_matrix, False))
-        transformed_target = self._warp_img(sample['target'],
-                                            transform_matrix, True)
+            transformed_inputs.append(self._rotate_image(original_input, code))
 
-        return {'inputs': transformed_inputs, 'target': transformed_target,
-                'id': sample['id']}
-    
-class MultibandDataset(torch.utils.data.Dataset):
+        transformed_target = self._rotate_image(sample['target'], code)
+        return {'inputs': transformed_inputs, 'target': transformed_target}
 
-    '''Torch dataset for loading multi-band images'''
 
-    @classmethod
-    def parse_classes(cls, classes):
-        '''Convert class string to list if necessary
+class ToTensor:
+    """Converts numpy ndarrays in sample to PyTorch Tensors with correct dimensions"""
+    def __call__(self, sample):
+        torch_inputs = []
+        for img in sample['inputs']:
+            # Handle single channel (H, W) depth maps vs (H, W, C) RGB images safely
+            if len(img.shape) == 2:
+                img = np.expand_dims(img, axis=2) # Convert (H, W) to (H, W, 1)
+            
+            # Change memory layout from HWC to CHW format expected by PyTorch
+            torch_inputs.append(torch.from_numpy(img.transpose((2, 0, 1))).float())
+        
+        # Ground truth targets MUST be integers (long) for PyTorch CrossEntropyLoss
+        target = torch.from_numpy(sample['target']).long()
+        
+        return {'inputs': torch_inputs, 'target': target}
 
+
+class MultibandDataset(Dataset):
+    """
+    Flexible dataset loader capable of managing single or multi-view streams
+    (e.g., loading an RGB directory stream alongside a Depth directory stream).
+    """
+    def __init__(self, img_paths, classes, id_file, gt_path=None, transform=None):
+        """
         Parameters
         ----------
-        classes : String or list of integer classes
-
-        Returns
-        -------
-        List of integer classes
-
-        '''
-        if isinstance(classes, str):
-            classes = [int(f) for f in classes.split(',')]
-
-        return classes
-
-    @classmethod
-    def _read_paths(cls, img_paths, selected, gt_path=None):
-        '''Create data structure to store paths to input and groundtruth images
-
-        Parameters
-        ----------
-        img_paths : List of paths to folders containing the respective band
-                    images
-        selected : File containing image names to be produced by dataset
-        gt_path : Path to the folder with the groundtruth images, optional
-
-        Returns
-        -------
-        List of image tuples ((band1_img, band2_img), gt_img, img_id)
-
-        '''
-        paths = []
-        with open(selected, 'r', encoding='UTF-8') as file:
-            selected_ids = [line.rstrip() for line in file]
-
-        for selected_id in selected_ids:
-            inputs = [os.path.join(img_path, f'{selected_id}.tif')
-                      for img_path in img_paths]
-            if gt_path is None:
-                target = None
-            else:
-                target = os.path.join(gt_path, f'{selected_id}.tif')
-            paths.append((inputs, target, selected_id))
-
-        # check that files exist
-        for inputs, target, _ in paths:
-            file_paths = [*inputs, target]
-            for file_path in file_paths:
-                if file_path is None:
-                    continue
-                assert os.path.isfile(file_path), f'Missing file: {file_path}'
-
-        return paths
-
-    def __init__(self, img_paths, classes, selected, gt_path=None,
-                 transform=None):
-        '''Configure dataset
-
-        Parameters
-        ----------
-        img_paths : List of paths to folders containing the respective band
-                    images
-        classes : String or list of integer class labels to be found in
-                  groundtruth images
-        selected : File containing image paths to be produced by dataset
-        gt_path : Path to the folder with the groundtruth images - classes need
-                  to be encoded by integers, optional
-        transform : Transform to be applied on a sample, optional
-
-        Returns
-        -------
-        Torch dataset
-
-        '''
-        self.paths = self._read_paths(img_paths, selected, gt_path)
-        self.classes = self.parse_classes(classes)
-        self.input_shape = self._infer_input_shape()
+        img_paths : List of folder paths (e.g., ['/path/to/rgb', '/path/to/depth'])
+        classes   : List of raw class integer labels (e.g., [0, 1, 2, 3...])
+        id_file   : TXT file containing specific image IDs for this CV split fold
+        """
+        self.img_paths = img_paths
+        self.classes = classes
+        self.gt_path = gt_path
         self.transform = transform
 
-        logging.info('Found %s images', len(self.paths))
-        logging.info("Assumed image shape: %s", self.input_shape)
-
-        torch.utils.data.Dataset.__init__(self)
-
-    def _infer_input_shape(self):
-        '''Infer input shape from ground truth image
-
-        Returns
-        -------
-        input shape
-
-        '''
-        # assume all images have the same shape
-        img = tifffile.imread(self.paths[0][0][0])
-
-        return (len(self.paths[0][0]), *img.shape)
+        # Load file names/IDs assigned to this cross-validation fold split
+        with open(id_file, 'r') as f:
+            self.ids = [line.strip() for line in f if line.strip()]
 
     def __len__(self):
-        '''Return number of images in the dataset
-        Returns
-        -------
-        Number of images
-
-        '''
-        return len(self.paths)
+        return len(self.ids)
 
     def __getitem__(self, idx):
-        '''Return input and groundtruth image corresponding to the given index
-
-        Parameters
-        ----------
-        idx : Index of the image to retrieve
-
-        Returns
-        -------
-        Requested sample dict {'inputs': ndarray, 'target': ndarray}
-
-        '''
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        input_paths, gt_path, img_id = self.paths[idx]
-
+        img_id = self.ids[idx]
         inputs = []
-        for input_path in input_paths:
-            inputs.append(tifffile.imread(input_path))
-        if gt_path is None:
-            target = np.nan
-        else:
-            target = tifffile.imread(gt_path)
 
-        sample = {'inputs': inputs, 'target': target, 'id': img_id}
+        # Gather arrays across all provided modality directories (RGB, Depth, etc.)
+        for folder in self.img_paths:
+            # Assumes files share structural names across folders (e.g., '0001.png')
+            full_path = os.path.join(folder, f"{img_id}.png")
+            # Read unchanged to preserve raw depth float/integer values
+            img = cv2.imread(full_path, cv2.IMREAD_UNCHANGED)
+            if img is None:
+                raise FileNotFoundError(f"Could not read input file: {full_path}")
+            inputs.append(img.astype(np.float32))
+
+        # Target mask retrieval
+        target = None
+        if self.gt_path:
+            gt_full_path = os.path.join(self.gt_path, f"{img_id}.png")
+            target = cv2.imread(gt_full_path, cv2.IMREAD_UNCHANGED)
+            if target is None:
+                raise FileNotFoundError(f"Could not read ground truth file: {gt_full_path}")
+
+        sample = {'inputs': inputs, 'target': target}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
 
-    def infer_weights(self, weighting):
-        '''Infer weigthing based on dataset and given weighting mode
-
-        Parameters
-        ----------
-        weighting : Weighting mode [none|mfb|weight string]
-
-        Returns
-        -------
-        Numpy array with one weight per class or None
-
-        '''
-        if weighting == 'none':
-            weights = None
-        elif weighting == 'mfb':
-            weights = self._infer_mfb_weights()
-        else:
-            weights = np.array([float(f) for f in weighting.split(',')])
-
-        logging.info('Set weights to: %s', weights)
-        return weights
-
-    def _infer_mfb_weights(self):
-        '''Calculate weights using median frequency balancing.
-           This approach has been proposed by Eigen and Fergus.
-           When there is an uneven number of classes, the weight for the class
-           with the median frequency is set to 1.0.
-
-           D. Eigen, and R. Fergus, "Predicting Depth, Surface Normals and
-           Semantic Labels With a Common Multi-Scale Convolutional
-           Architecture", 2015,
-
-        Returns
-        -------
-        list of class weights based on median frequency balancing
-
-        '''
-        weights = []
+    def calculate_median_frequency_weights(self):
+        """ Computes class balancing weights via Median Frequency Balancing (MFB) """
+        logging.info("Calculating Median Frequency Balancing Weights...")
         class_counts = {c: 0 for c in self.classes}
         class_totals = {c: 0 for c in self.classes}
 
-        for _, target_path, _ in self.paths:
-            target = tifffile.imread(target_path)
+        for img_id in self.ids:
+            gt_full_path = os.path.join(self.gt_path, f"{img_id}.png")
+            target = cv2.imread(gt_full_path, cv2.IMREAD_UNCHANGED)
             target_pixels = np.prod(target.shape)
+            
             for cls_idx in self.classes:
                 class_counts[cls_idx] += np.sum(target == cls_idx)
-                # only count the image if it contains at least one instance of
-                # the current class
                 if (target == cls_idx).any():
                     class_totals[cls_idx] += target_pixels
 
@@ -363,16 +144,10 @@ class MultibandDataset(torch.utils.data.Dataset):
             cnt = class_counts[cls_idx]
             tot = class_totals[cls_idx]
             if cnt == 0 and tot == 0:
-                logging.warning('WARN: No pixel of class %s found.', cls_idx)
-                # This case should not happen. However, if it does, setting the
-                # frequency to a small value ensures that the code can run.
-                frequencies.append(0.0001)
-            elif cnt > tot:
-                raise ValueError(f'Count mismatch class: {cls_idx} '
-                                 f'Count: {cnt} Total: {tot}')
+                frequencies.append(0.0001)  # Stability fallback for completely unrepresented classes
             else:
-                frequencies.append(cnt/tot)
+                frequencies.append(cnt / tot)
 
-        weights = [np.median(frequencies) / freq for freq in frequencies]
-
-        return np.array(weights)
+        median_freq = np.median(frequencies)
+        weights = [median_freq / freq for freq in frequencies]
+        return np.array(weights, dtype=np.float32)
