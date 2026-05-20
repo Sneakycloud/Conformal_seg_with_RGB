b381a0b8abd6d8b34c152540596a1ac8af1ca9be
Eddie Olofsgård <eddieolofsgard@icloud.com>
2026-05-20 16:13:37 +0200

    Added datapath for data

--- src/tools/controller.py
+++ src/tools/controller.py
@@ -11,6 +11,8 @@ import utils.util as util
 
 import h5py
 
+datapath = "../../data/nyu_v2/nyu_depth_v2_labeled.mat"
+#datapath = "../../../../home/data/epsilon/nyu_depth_v2_labeled.mat"
 
 #gets number of classes
 print("RGB training started")
@@ -19,5 +21,6 @@ print("D training started")
 train.rgbd_train("../../data/nyu_v2/nyu_depth_v2_labeled.mat", "../../logs", 42, 2, 64, class_list=torch.tensor([x for x in range(256)], mode="D"))
 print("RGBD training started")
 train.rgbd_train("../../data/nyu_v2/nyu_depth_v2_labeled.mat", "../../logs", 42, 2, 64, class_list=torch.tensor([x for x in range(256)], mode="RGBD"))
-print("RGBD TMC")
+print("TMC training started")
 train.rgbd_train("../../data/nyu_v2/nyu_depth_v2_labeled.mat", "../../logs", 42, 2, 64, class_list=torch.tensor([x for x in range(256)], mode="TMC"))
+print("All training completed")
